e901c13cb1057754bdf1a1c94eda1f6eb627ef7e
Thistledown <120768091@qq.com>
2018-08-11 12:05:21 +0800

    Initial commit
    Signed-off-by: Thistledown <120768091@qq.com>

---
+++ ch3/ex1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  :  Thistledown
+@Contact :  120768091@qq.com
+@Software:  PyCharm
+@File    :  ex1.py
+@Time    :  2018/8/11 12:12
+"""
+
+print('Hello, world!')
