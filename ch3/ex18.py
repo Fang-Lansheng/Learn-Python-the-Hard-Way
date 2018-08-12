cb40d92733c42976e477db02105ca97cb4667a6f
Thistledown <120768091@qq.com>
2018-08-12 11:33:04 +0800

    Initial commit
    Signed-off-by: Thistledown <120768091@qq.com>

---
+++ ch3/ex18.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  :  Thistledown
+@Contact :  120768091@qq.com
+@Software:  PyCharm
+@File    :  ex18.py
+@Time    :  2018/8/12 11:32
+"""
+'命名，变量，代码，函数'
+
+# this one is like 
