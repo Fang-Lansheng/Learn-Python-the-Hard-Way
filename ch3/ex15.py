2eb8a3bd5183e537c9dfb09352853ab89fd3ff86
Thistledown <120768091@qq.com>
2018-08-12 10:59:59 +0800

    Initial commit
    Signed-off-by: Thistledown <120768091@qq.com>

---
+++ ch3/ex15.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  :  Thistledown
+@Contact :  120768091@qq.com
+@Software:  PyCharm
+@File    :  ex15.py
+@Time    :  2018/8/12 10:56
+"""
+
+print('读文件')
