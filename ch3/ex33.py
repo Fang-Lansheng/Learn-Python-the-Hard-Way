710e48f4ca96baae56e3f904cf35db54227daf4b
Thistledown <120768091@qq.com>
2018-09-10 10:02:36 +0800

    Initial commit
    Signed-off-by: Thistledown <120768091@qq.com>

--- ch3/ex33.py
+++ ch3/ex33.py
@@ -7,3 +7,19 @@
 @File    :  ex33.py
 @Time    :  2018/9/9 19:22
 """
+'while循环'
+
+i = 0
+numbers = []
+
+while i < 6:
+    print('At the top i is %d' % i)
+    numbers.append(i)
+
+    i = i + 1
+    print('Numbers now:', numbers)
+    print('At the button i is %d' % i)
+
+print('The numbers:')
+for num in numbers:
+    print(num)
