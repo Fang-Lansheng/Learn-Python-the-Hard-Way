db71501ad80f51eb520920c908e93c519cd7e47b
Thistledown <120768091@qq.com>
2018-09-10 11:07:36 +0800

    Initial commit
    Signed-off-by: Thistledown <120768091@qq.com>

---
+++ ch3/ex34.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  :  Thistledown
+@Contact :  120768091@qq.com
+@Software:  PyCharm
+@File    :  ex34.py
+@Time    :  2018/9/10 10:02
+"""
+'访问列表元素'
+
