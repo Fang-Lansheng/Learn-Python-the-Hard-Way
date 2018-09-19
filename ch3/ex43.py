6dc82fb8293968d749aee7af24d6835d142d40f7
Thistledown <120768091@qq.com>
2018-09-19 20:12:57 +0800

    Thistledown
    Signed-off-by: Thistledown <120768091@qq.com>

---
+++ ch3/ex43.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  :  Thistledown
+@Contact :  120768091@qq.com
+@Software:  PyCharm
+@File    :  ex43.py
+@Time    :  2018/9/19 20:12
+"""
+'基本的面向对象分析和设计'
+
