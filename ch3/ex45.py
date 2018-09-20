7a8d7468d337b0d3bf16b7719f09bc2fdabd8e96
Thistledown <120768091@qq.com>
2018-09-20 18:44:53 +0800

    Thistledown
    Signed-off-by: Thistledown <120768091@qq.com>

---
+++ ch3/ex45.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  :  Thistledown
+@Contact :  120768091@qq.com
+@Software:  PyCharm
+@File    :  ex45.py
+@Time    :  2018/9/20 17:13
+"""
